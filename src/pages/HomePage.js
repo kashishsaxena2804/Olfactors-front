ef70049d15bdc9708dfacea765c06f2bc1424581
Kashish Saxena <kashish.saxena1306@gmail.com>
2024-03-19 14:15:32 +0530

    files

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -343,17 +343,18 @@ const HomePage = () => {
         </div>
         
         <div className='main-card1'>
-          <h2 className='heading-text'>Categories</h2>
+          <h2 className='heading-text'>Scent-Sational Discoveries</h2>
           <div className="row container">
-            {categories.map((c) => (
-              <div className="col-md-4 mt-5 mb-3 gx-3 gy-3" key={c._id}>
-                <div className="card">
-                  <Link to={`/category/${c.slug}`} className="btn cat-btn">
-                    {c.name}
-                  </Link>
-                </div>
+          {categories.map((c) => (
+            <div className="col-md-4 mt-5 mb-3 gx-3 gy-3" key={c._id}>
+              <div className="card">
+                <img src="./images/banner.jpeg" alt="" />
+                <Link to={`/category/${c.slug}`} className="btn cat-btn">
+                  {c.name}
+                </Link>
               </div>
-            ))}
+            </div>
+          ))}
           </div>
         </div>
 
