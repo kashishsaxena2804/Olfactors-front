93b5eb0b77b5b4e718c512bee4305289381a476d
Kashish Saxena <kashish.saxena1306@gmail.com>
2024-03-24 01:00:45 +0530

    files

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -348,8 +348,9 @@ const HomePage = () => {
           {categories.map((c) => (
             <div className="col-md-4 mt-5 mb-3 gx-3 gy-3" key={c._id}>
               <div className="card">
-                <img src={c.image}alt="" />
-                
+              <a href={`/category/${c.slug}`}>
+    <img src={c.image} alt="" />
+  </a>
               </div>
               <Link to={`/category/${c.slug}`} className="btn cat-btn">
                   {c.name}
