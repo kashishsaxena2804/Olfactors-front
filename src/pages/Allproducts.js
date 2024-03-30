a38751e0be0b10ffecf1bc4b0bfd3aa787296027
Kashish Saxena <kashish.saxena1306@gmail.com>
2024-03-30 21:51:14 +0530

    files

--- src/pages/Allproducts.js
+++ src/pages/Allproducts.js
@@ -146,48 +146,34 @@ const HomePage = () => {
           <h1 className="text-center">Explore More</h1>
           <div className="d-flex flex-wrap">
             {products?.map((p) => (
-              <div className="card m-2" key={p._id}>
-                <img
-                  src={`/api/v1/product/product-photo/${p._id}`}
+              <div className="product-card" onClick={() => navigate(`/product/${p.slug}`)}>
+                <img src={`/api/v1/product/product-photo/${p._id}`}
                   className="card-img-top"
-                  alt={p.name}
-                />
-                <div className="card-body">
-                  <div className="card-name-price">
-                    <h5 className="card-title">{p.name}</h5>
-                    <h5 className="card-title card-price">
-                      {p.price.toLocaleString("en-US", {
+                  alt={p.name}/>
+              <div className="product-details">
+                <h3 className="product-name">{p.name}</h3>
+                {/*<p className="product-offer">{p.offer}</p>*/}
+                <div className="product-prices">
+                  {/*<span className="original-price">500</span>*/}
+                  <span className="discounted-price">{p.price.toLocaleString("en-US", {
                         style: "currency",
                         currency: "USD",
-                      })}
-                    </h5>
-                  </div>
-                  <p className="card-text ">
-                    {p.description.substring(0, 60)}...
-                  </p>
-                  <div className="card-name-price">
-                    <button
-                      className="btn btn-info ms-1"
-                      onClick={() => navigate(`/product/${p.slug}`)}
-                    >
-                      More Details
-                    </button>
-                    <button
-                      className="btn btn-dark ms-1"
-                      onClick={() => {
+                      })}</span>
+                </div>
+                <button className="button add-to-cart-button" onClick={() => {
                         setCart([...cart, p]);
                         localStorage.setItem(
                           "cart",
                           JSON.stringify([...cart, p])
                         );
                         toast.success("Item Added to cart");
-                      }}
-                    >
-                      ADD TO CART
-                    </button>
-                  </div>
-                </div>
+                      }}>Add To Cart</button>
+                
               </div>
+            </div>
+
+
+              
             ))}
           </div>
           <div className="m-2 p-3">
