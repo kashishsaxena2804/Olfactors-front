4def01b7fbb9933a520c7f0bfc6245ba99e10c43
Kashish Saxena <kashish.saxena1306@gmail.com>
2024-04-04 00:05:21 +0530

    homepage fixes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -352,15 +352,18 @@ const HomePage = () => {
           <div className="main-card1">
             <h2 className="heading-text">Scent-Sational Discoveries</h2>
             <div className="row container">
-            {categories.map((c) => (
-            <div className="col-md-4 mt-5 mb-3 gx-3 gy-3" key={c._id}>
-              <div className="card">
-                <Link to={`/category/${c.slug}`} className="btn cat-btn">
-                  {c.name}
-                </Link>
-              </div>
-            </div>
-          ))}
+              {categories.map((c) => (
+                <div className="col-md-4 mt-5 mb-3 gx-3 gy-3" key={c._id}>
+                  <div className="card">
+                    <a href={`/category/${c.slug}`}>
+                      <img src={c.image} alt="" />
+                    </a>
+                  </div>
+                  <Link to={`/category/${c.slug}`} className="btn cat-btn">
+                    {c.name}
+                  </Link>
+                </div>
+              ))}
             </div>
           </div>
 
