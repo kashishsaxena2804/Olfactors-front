ea415c6844f03e47a7e5f3e023904b34c8656fde
Kashish Saxena <kashish.saxena1306@gmail.com>
2024-04-06 11:34:51 +0530

    cart page fix

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -82,7 +82,7 @@ const CartPage = () => {
   return (
     <Layout>
       <div className=" cart-page">
-        <div className="container">
+        <div className="cart-heading">
         <div className="row">
           <div className="col-md-12">
             <h1 className="text-center bg-light p-2 mb-1">
