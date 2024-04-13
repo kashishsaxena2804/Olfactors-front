2ecdc7a1f30dc7e49498a582ac5cb2ddaca93c9c
Kashish Saxena <kashish.saxena1306@gmail.com>
2024-04-13 19:17:45 +0530

    header fix

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -163,7 +163,7 @@ const HomePage = () => {
   }, []);
 
   return (
-    <Layout title={"All Products-Best offers"}>
+    <Layout title={"All Products"}>
       <div>
         <Carousel>
           <Carousel.Item>
@@ -173,38 +173,41 @@ const HomePage = () => {
               src={"./images/slider.jpg"}
               alt="First slide"
             />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
+          
           </Carousel.Item>
           <Carousel.Item>
             <img
               style={{ height: "90vh" }}
               className="d-block w-100"
-              src={"./images/slider.jpg"}
+              src={"./images/slider2.jpg"}
               alt="Second slide"
             />
 
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
+            
           </Carousel.Item>
           <Carousel.Item>
             <img
               style={{ height: "90vh" }}
               className="d-block w-100"
-              src={"./images/slider.jpg"}
+              src={"./images/slider3.jpg"}
               alt="Third slide"
             />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              style={{ height: "90vh" }}
+              className="d-block w-100"
+              src={"./images/slider4.jpg"}
+              alt="Third slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              style={{ height: "90vh" }}
+              className="d-block w-100"
+              src={"./images/slider5.jpg"}
+              alt="Third slide"
+            />
           </Carousel.Item>
         </Carousel>
       </div>
