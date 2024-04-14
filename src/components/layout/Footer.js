361d87d03afd241b2bfc463b2d5a2a441c69a916
Kashish Saxena <kashish.saxena1306@gmail.com>
2024-04-14 18:09:43 +0530

    icons in footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -72,12 +72,34 @@ const Footer = () => {
           </ul>
         </div>
         <div className="footer-section">
-          <h3 className="footer-heading">Contact Us</h3>
-          <p className="footer-content">
-            <a href="mailto:connect@aranyamperfumes.com" className="footer-link">theolfactors@gmail.com</a>
-          </p>
-          <p className="footer-content">Lucknow, Uttar Pradesh, India.</p>
-        </div>
+    <div class="flex-container">
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 14 14">
+    <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M4.5 4L7 6.5L9.5 4M7 .5v6"/>
+        <path d="M12 4.5a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-7a1 1 0 0 1 1-1"/>
+        <path d="M1 5.76L7 10l6-4.24"/>
+    </g>
+</svg>
+        <h4 className="footer-heading1">Have Doubts?</h4>
+    </div>
+    <p className="footer-content">
+        <a href="mailto:connect@aranyamperfumes.com" className="footer-link">theolfactors@gmail.com</a>
+    </p>
+
+    <div class="flex-container">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 14 14">
+            <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M3.519 6.746a1.75 1.75 0 1 0 3.5 0a1.75 1.75 0 1 0-3.5 0"/>
+                <path d="M9.629 8.74c-.948 2.542-3.366 4.756-4.36 4.756c-1.25 0-4.75-3.5-4.75-6.75a4.75 4.75 0 0 1 5.19-4.73M7.696 3.77c-.334-.059-.334-.538 0-.596a3.02 3.02 0 0 0 2.433-2.33l.02-.092c.072-.33.542-.331.617-.002l.024.107a3.036 3.036 0 0 0 2.44 2.316c.335.058.335.54 0 .598a3.036 3.036 0 0 0-2.44 2.316l-.024.107c-.075.329-.545.327-.617-.003l-.02-.092a3.02 3.02 0 0 0-2.433-2.33Z"/>
+            </g>
+        </svg>
+        <h4 className="footer-heading1">Find Us At</h4>
+    </div>
+    <p className="footer-content">Lucknow, Uttar Pradesh, India.</p>
+
+</div>
+
+
       </div>
       <div className="footer-copyright">
         <p className="footer-text">
