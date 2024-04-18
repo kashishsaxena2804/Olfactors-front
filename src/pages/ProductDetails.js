c04cb1b1e36f50b62c7f5b974682b4c6b48f8cbb
Kashish Saxena <kashish.saxena1306@gmail.com>
2024-04-18 09:47:38 +0530

    header positon fix

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -42,76 +42,71 @@ const ProductDetails = () => {
   };
   return (
     <Layout>
-      <div className="row container product-details mt-3">
-        <div className="col-md-6">
-          <img
-            src={`${process.env.REACT_APP_BASE_URL}/api/vl/product/product-photo/${product._id}`}
-            className="card-img-top"
-            alt={product.name}
-            height="500"
-            width={"350px"}
-          />
-        </div>
-        <div className="col-md-6 product-details-info">
-          <h1 className="text-center">Product Details</h1>
-          <hr />
-          <h6>Name : {product.name}</h6>
-          <h6>Description : {product.description}</h6>
-          <h6>
-            Price :
-            {product?.price?.toLocaleString("en-IN", {
-              style: "currency",
-              currency: "INR",
-            })}
-          </h6>
-          <h6>Category : {product?.category?.name}</h6>
-          <button class="button btn ">ADD TO CART</button>
-        </div>
-      </div>
-      <hr />
-      <div className="row container similar-products">
-        <h4>Similar Products  </h4>
-        {relatedProducts.length < 1 && (
-          <p className="text-center">No Similar Products found</p>
-        )}
-        <div className="d-flex flex-wrap">
-          {relatedProducts?.map((p) => (
-            <div className="product-card">
-            
-              <img
-                src={`${process.env.REACT_APP_BASE_URL}/api/vl/product/product-photo/${product._id}`}
-                className="card-img-top"
-                onClick={() => navigate(`/product/${p.slug}`)}
-                alt={p.name}
-              />
-              <div className="product-details">
-                <h3 className="product-name">{p.name}</h3>
-                {/*<p className="product-offer">{p.offer}</p>*/}
-                <div className="product-prices">
-                  {/*<span className="original-price">500</span>*/}
-                  <span className="discounted-price">
-                    {p.price.toLocaleString("en-IN", {
-                      style: "currency",
-                      currency: "INR",
-                    })}
-                  </span>
-                </div>
-                <button
-                        className="button add-to-cart-button"
-                        onClick={() => {
-                          setCart([...cart, p]);
-                          localStorage.setItem("cart", JSON.stringify([...cart, p]));
-                          toast.success("Item Added to cart");
-                        }}
-                      >
-                        Add To Cart
-                      </button>
-              </div>
-            
-            </div>
-          ))}
+      <div className="row product-details-container ">
+  <div className="col-md-6">
+    <img
+      src={`${process.env.REACT_APP_BASE_URL}/api/vl/product/product-photo/${product._id}`}
+      className="product-image"
+      alt={product.name}
+    />
+  </div>
+  <div className="col-md-6 product-details-info">
+    <h1 className="text-center">Product Details</h1>
+    <hr />
+    <h6>Name : {product.name}</h6>
+    <h6>Description : {product.description}</h6>
+    <h6>
+      Price :
+      {product?.price?.toLocaleString("en-IN", {
+        style: "currency",
+        currency: "INR",
+      })}
+    </h6>
+    <h6>Category : {product?.category?.name}</h6>
+    <button className="button btn">ADD TO CART</button>
+  </div>
+</div>
+<hr />
+<div className="row container similar-products">
+  <h4>Similar Products</h4>
+  {relatedProducts.length < 1 && (
+    <p className="text-center">No Similar Products found</p>
+  )}
+  <div className="d-flex flex-wrap">
+    {relatedProducts?.map((p) => (
+      <div className="product-card" key={p._id}>
+        <img
+          src={`${process.env.REACT_APP_BASE_URL}/api/vl/product/product-photo/${p._id}`}
+          className="product-image"
+          alt={p.name}
+          onClick={() => navigate(`/product/${p.slug}`)}
+        />
+        <div className="product-details">
+          <h3 className="product-name">{p.name}</h3>
+          <div className="product-prices">
+            <span className="discounted-price">
+              {p.price.toLocaleString("en-IN", {
+                style: "currency",
+                currency: "INR",
+              })}
+            </span>
+          </div>
+          <button
+            className="button add-to-cart-button"
+            onClick={() => {
+              setCart([...cart, p]);
+              localStorage.setItem("cart", JSON.stringify([...cart, p]));
+              toast.success("Item Added to cart");
+            }}
+          >
+            Add To Cart
+          </button>
         </div>
       </div>
+    ))}
+  </div>
+</div>
+
     </Layout>
   );
 };
