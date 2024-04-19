fd1f22766f368d9287e99e199d053857df5d3931
Kashish Saxena <kashish.saxena1306@gmail.com>
2024-04-19 16:49:05 +0530

    all product page fix

--- src/pages/CategoryProduct.js
+++ src/pages/CategoryProduct.js
@@ -7,6 +7,7 @@ import toast from "react-hot-toast";
 import { useCart } from "../context/cart";
 import { AiOutlineReload } from "react-icons/ai";
 import "../styles/CategoryProductStyles.css";
+import {Checkbox, Radio} from 'antd';
 
 const CategoryProduct = () => {
   const params = useParams();
@@ -17,11 +18,15 @@ const CategoryProduct = () => {
   const [total, setTotal] = useState(0);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [checked, setChecked] = useState([]);
+  const [radio, setRadio] = useState("");
 
   useEffect(() => {
-    if (params?.slug) getPrductsByCat();
+    if (params?.slug) getProductsByCat();
   }, [params?.slug]);
-  const getPrductsByCat = async () => {
+
+  const getProductsByCat = async () => {
     try {
       const { data } = await axios.get(
         `/api/vl/product/product-category/${params.slug}`
@@ -32,6 +37,7 @@ const CategoryProduct = () => {
       console.log(error);
     }
   };
+
   const getTotal = async () => {
     try {
       const { data } = await axios.get("/api/vl/product/product-count");
@@ -45,7 +51,7 @@ const CategoryProduct = () => {
     if (page === 1) return;
     loadMore();
   }, [page]);
-  //load more
+
   const loadMore = async () => {
     try {
       setLoading(true);
@@ -58,71 +64,162 @@ const CategoryProduct = () => {
     }
   };
 
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/vl/category/get-category");
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+    getTotal();
+  }, []);
+
+  const handleFilter = (value, id) => {
+    let all = [...checked];
+    if (value) {
+      all.push(id);
+    } else {
+      all = all.filter((c) => c !== id);
+    }
+    setChecked(all);
+  };
+
+  useEffect(() => {
+    if (!checked.length || !radio) getAllProducts();
+  }, [checked.length, radio]);
+
+  useEffect(() => {
+    if (checked.length || radio) filterProduct();
+  }, [checked, radio]);
+
+  const getAllProducts = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`/api/vl/product/product-list/${page}`);
+      setLoading(false);
+      setProducts(data.products);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  const filterProduct = async () => {
+    try {
+      const { data } = await axios.post("/api/vl/product/product-filters", {
+        checked,
+        radio,
+      });
+      setProducts(data?.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Layout>
       <div className="container mt-3 category">
+        <div className="col-md-3 filters">
+          <h4 className="text-center">Filter By Category</h4>
+          <div className="d-flex flex-column">
+            {categories?.map((c) => (
+              <Checkbox
+                key={c._id}
+                onChange={(e) => handleFilter(e.target.checked, c._id)}
+              >
+                {c.name}
+              </Checkbox>
+            ))}
+          </div>
+          <h4 className="text-center mt-4">Filter By Price</h4>
+          <div className="d-flex flex-column">
+            <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+              {Prices?.map((p) => (
+                <div key={p._id}>
+                  <Radio value={p.array}>{p.name}</Radio>
+                </div>
+              ))}
+            </Radio.Group>
+          </div>
+          <div className="d-flex flex-column">
+            <button
+              className="button btn"
+              onClick={() => window.location.reload()}
+            >
+              RESET FILTERS
+            </button>
+          </div>
+        </div>
         <h2 className="text-center">Category - {category?.name}</h2>
-        <h5 className="text-center">{products?.length} result found </h5>
-        
+        <h5 className="text-center">{products?.length} results found</h5>
         <div className="row">
           <div className="col-md-9 offset-1">
             <div className="d-flex flex-wrap">
-            {products?.map((p) => (
-              <div className="product-card" key={p._id} onClick={() => navigate(`/product/${p.slug}`)}>
-                <img
-                      src={`${process.env.REACT_APP_BASE_URL}/api/vl/product/product-photo/${p._id}`}
-                      className="card-img-top"
-                      onClick={() => navigate(`/product/${p.slug}`)}
-                      alt={p.name}
-                    />
-              <div className="product-details">
-                <h3 className="product-name">{p.name}</h3>
-                {/*<p className="product-offer">{p.offer}</p>*/}
-                <div className="product-prices">
-                  {/*<span className="original-price">500</span>*/}
-                  <span className="discounted-price">
-                          {p.price.toLocaleString("en-IN", {
-                            style: "currency",
-                            currency: "INR",
-                          })}
-                        </span>
-                </div>
-                <button className="button add-to-cart-button" onClick={() => {
+              {products?.map((p) => (
+                <div
+                  className="product-card"
+                  key={p._id}
+                  onClick={() => navigate(`/product/${p.slug}`)}
+                >
+                  <img
+                    src={`${process.env.REACT_APP_BASE_URL}/api/vl/product/product-photo/${p._id}`}
+                    className="card-img-top"
+                    onClick={() => navigate(`/product/${p.slug}`)}
+                    alt={p.name}
+                  />
+                  <div className="product-details">
+                    <h3 className="product-name">{p.name}</h3>
+                    <div className="product-prices">
+                      <span className="discounted-price">
+                        {p.price.toLocaleString("en-IN", {
+                          style: "currency",
+                          currency: "INR",
+                        })}
+                      </span>
+                    </div>
+                    <button
+                      className="button add-to-cart-button"
+                      onClick={() => {
                         setCart([...cart, p]);
                         localStorage.setItem(
                           "cart",
                           JSON.stringify([...cart, p])
                         );
                         toast.success("Item Added to cart");
-                      }}>Add To Cart</button>
-                
-              </div>
-            </div>
-
-
-              
-               ))}
+                      }}
+                    >
+                      Add To Cart
+                    </button>
+                  </div>
+                </div>
+              ))}
             </div>
             <div className="m-2 p-3">
-            {products && products.length < total && (
-              <button
-                className="btn loadmore"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setPage(page + 1);
-                }}
-              >
-                {loading ? (
-                  "Loading ..."
-                ) : (
-                  <>
-                    {" "}
-                    Loadmore <AiOutlineReload />
-                  </>
-                )}
-              </button>
-            )}
-          </div>
+              {products && products.length < total && (
+                <button
+                  className="btn loadmore"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setPage(page + 1);
+                  }}
+                >
+                  {loading ? (
+                    "Loading ..."
+                  ) : (
+                    <>
+                      {" "}
+                      Loadmore <AiOutlineReload />
+                    </>
+                  )}
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
