ea5759b602751ed2715b605bd9b0a0aa47bc9115
Kashish Saxena <kashish.saxena1306@gmail.com>
2024-04-21 01:24:50 +0530

    all product page fix

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -56,15 +56,15 @@ const ProductDetails = () => {
         <div className="col-md-6 product-details-info">
           <h1 className="text-center">Product Details</h1>
           <hr />
-          <h6>Name : {product.name}</h6>
-          <h6>Description : {product.description}</h6>
-          <h6>
-            Price :
+          <h4 className="product-name1">Name{product.name}</h4>
+          <h5>Description : {product.description}</h5>
+          <h5>
+            
             {product?.price?.toLocaleString("en-IN", {
               style: "currency",
               currency: "INR",
             })}
-          </h6>
+          </h5>
           <h6>Category : {product?.category?.name}</h6>
           <button
             className="button add-to-cart-button"
